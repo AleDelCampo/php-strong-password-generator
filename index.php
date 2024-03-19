15662630c3c209f6b135f24075ca9a0c2f90debc
AleHashirama <alehashirama@gmail.com>
2024-03-19 16:09:17 +0100

    Milestone 1

--- index.php
+++ index.php
@@ -1,3 +1,37 @@
+<?php
+
+function generateRandomPassword($length) {
+
+
+    $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+{}|:<>?';
+
+    $charsLength = strlen($chars);
+
+    $password = '';
+
+    for ($i = 0; $i < $length; $i++) {
+        $randomChar = $chars[rand(0, $charsLength - 1)];
+        
+        $password .= $randomChar;
+    }
+
+    return $password;
+}
+
+
+if (isset($_GET['passLength'])) {
+
+    $passLength = $_GET['passLength'];
+
+    $randomPassword = generateRandomPassword($passLength);
+
+} else {
+
+    $randomPassword = generateRandomPassword(8);
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,22 +40,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP-Password-Maker</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
 
-    
+    <form class="container">
+
+        <div class="form-group mt-2">
+
+            <label for="vote" class="mb-2">Quanto vuoi sia lunga la tua password??:</label>
+
+            <input type="number" id="passLength" name="passLength" placeholder="">
+            
+            <?php echo $randomPassword ?>
+
+        </div>
+
+        <button type="submit" class="btn border-black">Submit</button>
+
+    </form>
 
 
 
-
-
-
-
-
-    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
 </body>
 
 </html>
